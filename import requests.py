844cc7a2d83a26430e2d7588eba91b57fa162a35
Sumeet Pandey <123485996+sumeet3353@users.noreply.github.com>
2023-03-30 21:23:42 +0530

    Create import requests.py

---
+++ import requests.py
@@ -0,0 +1,37 @@
+import requests
+import json
+
+api_key = "YOUR_API_KEY_HERE"
+
+
+base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+
+def get_weather(city):
+    
+    complete_url = base_url + "appid=" + api_key + "&q=" + city
+
+    
+    response = requests.get(complete_url)
+
+    
+    data = response.json()
+
+    
+    if data["cod"] == "404":
+        return "City not found. Please enter a valid city name."
+
+    
+    weather = data["weather"][0]["description"]
+    temperature = round(data["main"]["temp"] - 273.15, 2) # convert Kelvin to Celsius
+    humidity = data["main"]["humidity"]
+
+    
+    message = f"Current weather in {city}: {weather}, Temperature: {temperature}°C, Humidity: {humidity}%."
+
+    return message
+
+
+if __name__ == '__main__':
+    city = input("Enter city name: ")
+    print(get_weather(city))
